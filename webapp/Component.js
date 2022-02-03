5ccd65c79b5a866399b6698c2a9db9b1b9aab9e5
Jay Mukherjee <Jay_Mukherjee@contractor.murphyoilcorp.com>
2022-02-03 17:22:35 +0000

    Version increased.

--- webapp/Component.js
+++ webapp/Component.js
@@ -19,7 +19,7 @@ sap.ui.define([
 		 */
 		init: function () {
 			//Please dont delete this  below line, and whenever new build you are doing please update the version as well eq : 1.0.2
-			console.log('Version : 1.0.1');
+			console.log('Version : 1.0.2');
 			this.serviceCall = new ServiceCall();
 			
 			// call the base component's init function
