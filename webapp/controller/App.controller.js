723d5e0836dbe30fd95bd867ecb67a4ad1dca06c
Jay Mukherjee <Jay_Mukherjee@contractor.murphyoilcorp.com>
2022-02-04 17:22:39 +0000

    Pre UAT Issues fixed

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -5,7 +5,8 @@ sap.ui.define([
 
 	return Controller.extend("murphy.mdm.vendor.murphymdmvendor.controller.View1", {
 		onInit: function () {
-
+				//Please dont delete this  below line, and whenever new build you are doing please update the version as well eq : 1.0.2
+			console.log('Version : 1.0.3');
 		}
 	});
 });
