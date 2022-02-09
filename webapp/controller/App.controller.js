884b596d28e68c5b17c19a14cb8ca2b9087cd099
Jay Mukherjee <Jay_Mukherjee@contractor.murphyoilcorp.com>
2022-02-09 17:12:51 +0000

    fix

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -6,7 +6,7 @@ sap.ui.define([
 	return Controller.extend("murphy.mdm.vendor.murphymdmvendor.controller.View1", {
 		onInit: function () {
 				//Please dont delete this  below line, and whenever new build you are doing please update the version as well eq : 1.0.2
-			console.log('Version : 1.0.4');
+			console.log('Version : 1.0.5');
 			this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
 		}
 	});
